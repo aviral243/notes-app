4faad83dcd4823530620b9afa38d727e056181b9
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-11-28 16:30:44 +0530

    Intialized the app

---
+++ app.js
@@ -0,0 +1,67 @@
+const chalk = require("chalk");
+const yargs = require("yargs");
+const notes = require("./notes");
+
+//Customize yargs version
+yargs.version("1.1.0");
+
+// Create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    },
+    body: {
+      describe: "Note body",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    notes.addNote(argv.title, argv.body);
+  }
+});
+
+// Create remove command
+yargs.command({
+  command: "remove",
+  describe: "Remove a note",
+  builder: {
+    title: {
+      describe: "Note title",
+      demandOption: true,
+      type: "string"
+    }
+  },
+  handler: function(argv) {
+    notes.removeNote(argv.title);
+  }
+});
+
+// Create list command
+
+yargs.command({
+  command: "list",
+  describe: "Listing all notes",
+  handler: function() {
+    console.log("Listing all notes");
+  }
+});
+
+// Create read command
+
+yargs.command({
+  command: "read",
+  describe: "Reading notes",
+  handler: function() {
+    console.log("Reading notes");
+  }
+});
+
+yargs.parse();
+
+// console.log(yargs.argv)
