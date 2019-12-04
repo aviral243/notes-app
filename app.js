02619afd84e7236f0d4f874d11e564a73f2fed0f
Aviral Gangwar <aviralgangwar24@gmail.com>
2019-12-05 03:39:20 +0530

    Refactor all functions to ES6

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ yargs.command({
       type: "string"
     }
   },
-  handler: function(argv) {
+  handler: argv => {
     notes.addNote(argv.title, argv.body);
   }
 });
@@ -37,7 +37,7 @@ yargs.command({
       type: "string"
     }
   },
-  handler: function(argv) {
+  handler: argv => {
     notes.removeNote(argv.title);
   }
 });
@@ -47,7 +47,7 @@ yargs.command({
 yargs.command({
   command: "list",
   describe: "Listing all notes",
-  handler: function() {
+  handler: () => {
     console.log("Listing all notes");
   }
 });
@@ -57,7 +57,7 @@ yargs.command({
 yargs.command({
   command: "read",
   describe: "Reading notes",
-  handler: function() {
+  handler: () => {
     console.log("Reading notes");
   }
 });
